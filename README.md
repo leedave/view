9004f8a623fa56af196d74aaf4eb8c61dab9021c
David Lee <leed@leed.ch>
2018-01-01 23:20:27 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# View Generator #
+
+A simple library that can generate a HTML file from any text file. 
+Supports variables and translations
+
+Add Variables by putting {{$VARIABLENAME}} in your Template
+Add Translations by putting {{t:translationName}} in your Templates
+
+An example template is in the folder /examples
---
+++ src/View.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Leedch\View;
+
+use Leedch\Translate\Translate as T;
+
+/**
+ * Class for loading views from templates
+ *
+ * @author leed
+ */
+class View
+{
+    protected $translations;
+    
+    /**
+     * Loads a Template, inserts Attributes in {{$ }}
+     * @param string $file
+     * @param array $attributes
+     * @return string
+     */
+    public function loadView(string $file, array $attributes = []) : string
+    {
+        $content = file_get_contents($file);
+        $translator = T::getInstance();
+        $arrTranslations = $translator->getTranslations();
+        foreach ($attributes as $key => $val) {
+            $content = str_replace('{{$'.$key.'}}', $val, $content);
+        }
+        foreach ($arrTranslations as $key => $val) {
+            $content = str_replace('{{t:'.$key.'}}', $val, $content);
+        }
+        return $content;
+    }
+}
